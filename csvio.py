9ce21bb544427b802a447326ff5abb7a9e246f3e
Anatolij Zelenin <az@azapps.de>
2013-02-07 22:42:56 +0100

    removed the class and some small improvements

--- csvio.py
+++ csvio.py
@@ -1,38 +1,30 @@
-#!/usr/bin/python2.7
 #coding=utf-8
 import numpy
-class CSVIO(object):
-    _delimiter=','
-    _newline='\n'
-    def __init__(self,fname):
-        """Initialize the CSV file
-        @TODO The delimiters should be extracted automatically from the file
-        """
-        self.fname=fname
-    def read(self,names=True):
-        """Return a 2-dimensional numpy-Array with the content of the CSV-file
+_delimiter=','
+_newline='\n'
+def read(fname,names=True):
+    """Return a 2-dimensional numpy-Array with the content of the CSV-file
 
-        Keyword arguments:
-        names -- Should the names be extracted from the first line of the CSV-file and saved in the dtype?
-        """
-        return numpy.genfromtxt(self.fname,delimiter=self._delimiter,names=names,dtype=None,invalid_raise=False)
-    def write(self,fname,data,names=True):
-        """Write a numpy-array to a CSV-file
+    Keyword arguments:
+    names -- Should the names be extracted from the first line of the CSV-file and saved in the dtype?
+    """
+    return numpy.genfromtxt(fname,delimiter=_delimiter,names=names,dtype=None,invalid_raise=False)
+def write(fname,data,namesFromArray=True):
+    """Write a numpy-array to a CSV-file
 
-        Keyword arguments:
-        fname -- filename
-        data -- The numpy-array
-        names -- Should the names be extracted from the array and be written in the first line of the CSV-file?
-        """
-        write=[]
-        if names==True:
-            names=[]
-            for tup in data.dtype.descr:
-                names.append(tup[0])
-            write.append(self._delimiter.join(names))
-        for row in data:
-            write.append(self._delimiter.join(map(str,row)))
-        handle=file(fname,'w')
+    Keyword arguments:
+    fname -- filename
+    data -- The numpy-array
+    namesFromArray -- Should the names be extracted from the array and be written in the first line of the CSV-file?
+    """
+    write=[]
+    if namesFromArray==True:
+        names=[]
+        for tup in data.dtype.descr:
+            names.append(tup[0])
+        write.append(_delimiter.join(names))
+    for row in data:
+        write.append(_delimiter.join(map(str,row)))
+    with file(fname,'w') as handle:
         for r in write:
-            handle.write(r+self._newline)
-        handle.close()
+            handle.write(r+_newline)
